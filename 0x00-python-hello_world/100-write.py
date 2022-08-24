07f15e6d70fd0471db3c5b63bc75808340d52bb7
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-24 11:55:09 +0000

    task 100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
 
+
 def write_to_stderr(*a):
     sys.stderr.write(*a)
+
 
 write_to_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
