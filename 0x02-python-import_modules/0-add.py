5076e0db21e4ea4fb2782054940afcb62feb030d
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2022-08-25 19:25:21 +0000

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
+from add_0 import add as add_it
 
 
 a = 1
 b = 2
-print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
+print("{0:d} + {1:d} = {2:d}".format(a, b, add_it(a, b)))
