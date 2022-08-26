c82fc4e7ed64880b3b68101404a3f893c39528df
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-26 05:58:45 -0700

    Update task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,40 +5,34 @@ if __name__ == "__main__":
     from sys import argv
 
 
-# variables to be used in program
-    y = argv[1]
-    x = argv[3]
-    a = int(y)
-    b = int(x)
+# variables for initialization    
     error_arg_msg = 'Usage: ./100-my_calculator.py <a> <operator> <b>'
     error_opr_msg = 'Unknown operator. Available operators: +, -, * and /'
     opr = ['+', '-', '*', '/']
-    c = add(a, b)
-    d = sub(a, b)
-    e = mul(a, b)
-    f = div(a, b)
 
+ # Initial Conditions for calculator to work
+    num_args = len(argv)
+    if num_args != 3:
+        print(error_arg_msg)
+        exit(1)
 
-# Initial Conditions for calculator to work
-    for i in range(len(argv)):
-        if i != 3:
-            print(error_arg_msg)
-            exit(1)
-        elif (
-                argv[2] == opr[0] or
-                argv[2] == opr[1] or
-                argv[2] == opr[2] or
-                argv[2] == opr[3]
-                ):
-            print(error_opr_msg)
-            exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        c = add(a, b)
+        d = sub(a, b)
+        e = mul(a, b)
+        f = div(a, b)
 
 # Calculate the Arguments
-    if argv[2] == opr[0]:  # Addition operation
-        print("{0:d} + {1:d} = {2:d}".format(a, b, c))
-    elif argv[2] == opr[1]:  # Subtraction operation
-        print("{0:d} - {1:d} = {2:d}".format(a, b, d))
-    elif argv[2] == opr[2]:  # Multiplication operation
-        print("{0:d} * {1:d} = {2:d}".format(a, b, e))
-    elif argv[3] == opr[3]:  # Division operation
-        print("{0:d} / {1:d} = {2:d}".format(a, b, f))
+        if argv[2] == opr[0]:  # Addition operation
+            print("{0:d} + {1:d} = {2:d}".format(a, b, c))
+        elif argv[2] == opr[1]:  # Subtraction operation
+            print("{0:d} - {1:d} = {2:d}".format(a, b, d))
+        elif argv[2] == opr[2]:  # Multiplication operation
+            print("{0:d} * {1:d} = {2:d}".format(a, b, e))
+        elif argv[2] == opr[3]:  # Division operation
+            print("{0:d} / {1:d} = {2:d}".format(a, b, f))
+        else:
+            print(error_opr_msg)
+            exit(1)
