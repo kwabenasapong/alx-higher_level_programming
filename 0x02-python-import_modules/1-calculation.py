9b1d02b22d2e82c32e6c1eb0b17216f7d230d418
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-26 00:16:09 +0000

    Add task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    c = add(a, b)
+    d = sub(a, b)
+    e = mul(a, b)
+    f = div(a, b)
+
+    print("{0:d} + {1:d} = {2:d}".format(a, b, c))
+    print("{0:d} + {1:d} = {2:d}".format(a, b, d))
+    print("{0:d} + {1:d} = {2:d}".format(a, b, e))
+    print("{0:d} + {1:d} = {2:d}".format(a, b, f))
