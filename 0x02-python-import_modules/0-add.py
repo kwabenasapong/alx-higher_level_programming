893cc1baeee2af01bb33c1f0be6eac43911b9c3c
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-26 00:05:15 +0000

    Update task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-from add_0 import add as add_it
+    from add_0 import add as add_it
 
-a = 1
-b = 2
-c = add_it(a, b)
-print("{0:d} + {1:d} = {2:d}".format(a, b, c))
+    a = 1
+    b = 2
+    c = add_it(a, b)
+    print("{0:d} + {1:d} = {2:d}".format(a, b, c))
