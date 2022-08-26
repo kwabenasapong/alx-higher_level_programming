bff136668e80090a5874587ef31a38011143ef9a
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-26 00:20:39 +0000

    Update task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     f = div(a, b)
 
     print("{0:d} + {1:d} = {2:d}".format(a, b, c))
-    print("{0:d} + {1:d} = {2:d}".format(a, b, d))
-    print("{0:d} + {1:d} = {2:d}".format(a, b, e))
-    print("{0:d} + {1:d} = {2:d}".format(a, b, f))
+    print("{0:d} - {1:d} = {2:d}".format(a, b, d))
+    print("{0:d} * {1:d} = {2:d}".format(a, b, e))
+    print("{0:d} / {1:d} = {2:d}".format(a, b, f))
