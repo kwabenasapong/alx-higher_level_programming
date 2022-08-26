7ddcd665d07ee9ebcb1eec50236767a8d899d31b
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-26 03:13:29 +0000

    Add task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+'''
+def count_args(str):
+    for count in range(len(str)):
+        continue
+    return count
+'''
+
+def sum_args(str):
+    sum = 0
+    for arg in range(len(str)):
+        if arg != 0:
+            num = int(str[arg])
+            sum += num
+    return sum
+
+
+if __name__ == "__main__":
+
+    '''
+    num_args = count_args(argv)
+    if num_args == 1:
+        message = 'argument:'
+    elif num_args == 0:
+        message = 'arguments.'
+    else:
+        message = 'arguments:'
+    '''
+
+    #print("{0:d} {1:s}".format(num_args, message))
+    print(sum_args(argv))
