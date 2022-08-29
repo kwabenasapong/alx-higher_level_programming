cd994ff750b311a0b9193c7ba2a2148e92c9696d
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-29 12:04:17 +0000

    Add task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+a function that replaces an element in a list at a 
+specific position without modifying the original list 
+'''
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+    copy_list = my_list.copy()
+    for elements in range(len(copy_list)):
+        if idx == elements:
+            copy_list[idx] = element
+            return copy_list
