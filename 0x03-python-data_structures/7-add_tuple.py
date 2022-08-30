a95ad8fd3a76fcbc569c7e53f8291536e8b4f686
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-30 04:13:02 +0000

    Update task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,11 +3,12 @@
 a function that adds 2 tuples
 '''
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    a= tuple_a
+    a = tuple_a
     b = tuple_b
     if (len(a) < 2 and len(a) == 1):
-            a = (a[0], 0)
+        a = (a[0], 0)
     elif len(a) == 0:
         a = (0, 0)
     elif (len(b) < 2 and len(b) == 1):
