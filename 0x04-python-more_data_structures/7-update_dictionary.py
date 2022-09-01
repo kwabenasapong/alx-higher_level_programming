207ef4611731562c742e1b470f09f8a7c270b77e
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-01 04:25:22 +0000

    add task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    a_dictionary[key] = value
+    return a_dictionary
