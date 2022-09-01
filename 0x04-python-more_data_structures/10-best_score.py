0359c0dc56d971e4d4e43fe9b543578e551c12bb
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-01 11:59:18 +0000

    Update task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return a_dictionary
+    if a_dictionary == {}:
+        return None
     max_key = max(a_dictionary)
     return max_key
