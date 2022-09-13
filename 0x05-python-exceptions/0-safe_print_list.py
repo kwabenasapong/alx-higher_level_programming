ac68b3a66ae91e34db08504ec5ad0560ed5519bc
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-13 09:35:25 +0000

    Add task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!\usr\bin\python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    try:
-        y = my_list[:x]
-    except IndexError:
-        pass
-    else:
-        for a in y:
+    y = my_list[:x]
+    for a in y:
+        try:
             i += 1
             print("{}".format(a), end='')
-            print()
+
+        except IndexError:
+            break
+    print()
     return (i)
