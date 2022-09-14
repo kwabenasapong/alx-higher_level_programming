deaaf09b90d272a12560394fc9fbfa03d73848f3
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-14 15:39:56 +0000

    work on task 5

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,8 @@ def list_division(my_list_1, my_list_2, list_length):
     y = len(my_list_2)
 
     try:
-        z = [my_list_1[a]/my_list_2[b] for a in range(x) for b in range(y) if a == b]
+        z = [my_list_1[a]/my_list_2[b]
+             for a in range(x) for b in range(y) if a == b]
         list_length = len(z)
 
     except ZeroDivisionError:
