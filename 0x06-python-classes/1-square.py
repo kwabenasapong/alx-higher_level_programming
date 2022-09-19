91c70745e910154f80821d9ce85c9d180504581e
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-19 13:38:46 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''Square Class'''
+
+
+class Square:
+    '''A class
+    that defines a square
+    '''
+    def __init__(self, size):
+        '''__init__method for class Square
+
+        Args:
+            param1 (size): private instance attribute
+        '''
+
+        self.__size = size
