83a2a53641510ffe61f4d80f7517b2de04bde3c4
kwabenasapong <kwabenasapong@gmail.com>
2022-09-20 02:24:21 +0000

    Add task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+'''Square Class'''
+
+
+class Square:
+    '''A class
+    that defines a square
+    '''
+    def __init__(self, size=0):
+        '''__init__method for class Square
+
+        Args:
+            param1 (size): private instance attribute
+        '''
+
+        if not type(size) is int:
+            message_1 = "size must be an integer"
+            raise TypeError(message_1)
+        if size < 0:
+            message_2 = "size must be >= 0"
+            raise ValueError(message_2)
+
+        self.__size = size
+
+    def area(self):
+        '''Public instance method:
+        def area(self): that returns
+        the current square area
+        '''
+        return (self.__size) ** 2
