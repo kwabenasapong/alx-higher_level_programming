efc5972b097ec470a2c9edf54a26507dfdb7cbfd
kwabenasapong <kwabenasapong@gmail.com>
2022-09-20 02:12:41 +0000

    Update task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -18,7 +18,7 @@ class Square:
             message_1 = "size must be an integer"
             raise TypeError(message_1)
         if size < 0:
-            message_2 = "size must be >=0"
+            message_2 = "size must be >= 0"
             raise ValueError(message_2)
 
         self.__size = size
