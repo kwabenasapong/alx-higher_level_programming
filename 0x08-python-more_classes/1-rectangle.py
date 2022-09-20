7920360ded037d4b672dc0b706174f7cfb1a0a63
kwabenasapong <kwabenasapong@gmail.com>
2022-09-20 05:10:32 +0000

    Update task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
         param2 height: rectangle height
         '''
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
