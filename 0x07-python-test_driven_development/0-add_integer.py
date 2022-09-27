fdec20cfd95cf6211dbb2bee3d6418e8d9651c17
kwabenasapong <kwabenasapong@gmail.com>
2022-09-27 01:28:29 +0000

    Add Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+'''The float and int addition module'''
+
+
 def add_integer(a, b=98):
     '''a function that adds only 2 int or float type only'''
-    if not isinstance(a, int) and not isinstance(a, float):
+    if a is None or (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError('a must be an integer')
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
