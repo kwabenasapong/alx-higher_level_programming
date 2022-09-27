2cbadde6ea393a1f9f9fafc680378ecb48598d39
kwabenasapong <kwabenasapong@gmail.com>
2022-09-27 04:07:51 +0000

    Add Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''MyList subclass'''
+
+
+class MyList(list):
+    '''Inherits from base class list'''
+
+
+    def print_sorted(self):
+        '''
+        Public instance method:
+        that prints the list, but sorted (ascending sort)
+        '''
+        sorted_list = []
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list)
