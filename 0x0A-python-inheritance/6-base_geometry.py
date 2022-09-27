efeed1434bb0366b1cb7cc4d1206b939c215ad5e
kwabenasapong <kwabenasapong@gmail.com>
2022-09-27 05:39:19 +0000

    Add Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''BaseGeometry Class'''
+
+
+class BaseGeometry:
+    '''A class
+    that defines a BaseGeometry
+    '''
+
+    def __init__(self):
+        '''initialize class'''
+        pass
+
+    def area(self):
+        '''Area of Geometric figure'''
+        raise Exception('area() is not implemented')
