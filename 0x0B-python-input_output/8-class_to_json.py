00f076f6fa3a5aa594df7b56eb306ce5cc98d18e
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-29 02:38:22 -0700

    Add task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Class to Object Module'''
+def class_to_json(obj):
+    '''
+    function that returns the dict
+    description with simple data 
+    structure
+    '''
+    return (type(obj))
