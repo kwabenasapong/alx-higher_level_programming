19e23f2ea5ddfab457255d5dbae02ba225efa3a8
kwabenasapong <kwabenasapong@gmail.com>
2022-10-01 15:18:23 +0000

    Update Task 9: corrected indents

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,20 @@
+#!/usr/bin/python3
+
 class Student:
-	'''Student Class'''
+    '''Student Class'''
 
-	first_name = ''
-	last_name = ''
-	age = 0
+    first_name = ''
+    last_name = ''
+    age = 0
 
-	def __init__(self, first_name, last_name, age):
-		'''initialse attributes'''
-		self.first_name = first_name
-		self.last_name = last_name
-		self.age = age
+    def __init__(self, first_name, last_name, age):
+        '''initialse attributes'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-	def to_json(self):
-		'''retrieves a dictionary
-		representation of a Student
-		instance '''
-		return vars(self)
+    def to_json(self):
+        '''retrieves a dictionary
+        representation of a Student
+        instance '''
+        return vars(self)
