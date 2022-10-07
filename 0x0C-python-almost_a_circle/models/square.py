8d48a1103129b18e21c86f3a8a2a671f896f4055
Kwabena Sapong <kwabenasapong@gmail.com>
2022-10-07 02:08:09 -0700

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''initialize square class'''
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         '''print message'''
@@ -25,3 +26,21 @@ class Square(Rectangle):
         '''setter for size'''
         self.width = value
         self.height = value
+
+def update(self, *args, **kwargs):
+    '''for updating all initialised args'''
+    if args is not None and len(args) != 0:
+        for i in range(len(args)):
+            if i == 0:
+                self.id = args[0]
+            elif i == 1:
+                self.size = args[1]
+            elif i == 2:
+                self.x = args[2]
+            else:
+                self.y = args[3]
+    else:
+        keys = ['size', 'x', 'y', 'id']
+        for k in kwargs.keys():
+            if k in keys:
+                setattr(self, k, kwargs[k])
