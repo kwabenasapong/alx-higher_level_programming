343e7884c172a492140037ca7b83c9881365c0d0
Kwabena Sapong <kwabenasapong@gmail.com>
2022-11-04 11:41:26 +0000

    Add task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,3 +4,6 @@
 -- 1. Database hbtn_0c_0
 -- 2. Table first_table
 -- 3. Field name in first_table
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
