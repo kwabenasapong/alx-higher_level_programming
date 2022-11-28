7724071fb657c1b624edf3ba68f78af55a905484
kwabenasapong <kwabenasapong@gmail.com>
2022-11-28 23:38:41 +0000

    Add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/*
+    Prints the two argument
+    passed to it
+*/
+function checkArg (x, base) {
+  const number = parseInt(x, base);
+  if (isNaN(number)) { return 0; }
+  return number;
+}
+
+if (process.argv.length <= 2 || checkArg(process.argv[2], 10) === 0) {
+  console.log('Missing size');
+  process.exit(1);
+} else {
+  const x = checkArg(process.argv[2], 10);
+  let i = 0;
+  let j = 0;
+  let pattern = '';
+  while (i < x) {
+    while (j < x) {
+      pattern += 'x';
+      j++;
+    }
+    console.log(pattern);
+    i++;
+  }
+}
