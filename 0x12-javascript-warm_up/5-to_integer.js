e34508ebfe7f31d3cadd7623d264db6251cb646a
kwabenasapong <kwabenasapong@gmail.com>
2022-11-28 21:36:03 +0000

    Add task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+    Prints the two argument
+    passed to it
+*/
+function checkArg (x, base) {
+  const number = parseInt(x, base);
+  if (isNaN(number)) { return 0; }
+  return number;
+}
+
+if (process.argv.length <= 2) {
+  console.log('Not a number');
+  process.exit(1);
+}
+
+if (checkArg(process.argv[2], 10) === 0) {
+  console.log('Not a number');
+  process.exit(1);
+} else {
+  console.log(checkArg(process.argv[2], 10));
+}
