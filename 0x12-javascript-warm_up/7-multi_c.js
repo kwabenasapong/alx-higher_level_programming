2334f8d48695148419fb3f3e074c6d000e124837
kwabenasapong <kwabenasapong@gmail.com>
2022-11-28 22:21:12 +0000

    Add task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/*
+    Prints the two argument
+    passed to it
+*/
+function checkArg (x, base) {
+  const number = parseInt(x, base);
+  if (isNaN(number)) { return 0; }
+  return number;
+}
+
+if (process.argv.length <= 2) {
+  console.log('Missing number of occurrences');
+  process.exit(1);
+}
+
+if (checkArg(process.argv[2], 10) === 0) {
+  console.log('Missing number of occurrences');
+  process.exit(1);
+} else {
+  const x = checkArg(process.argv[2], 10);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
