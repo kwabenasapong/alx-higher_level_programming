cf71fe005e69f77de9ddb87095e6cc981a878688
kwabenasapong <kwabenasapong@gmail.com>
2022-11-29 15:38:56 +0000

    Update 101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const callMeMoby = function (x, theFunction) {};
 
-callMeMoby.prototype.log = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
 };
-exports.callMeMoby = callMeMoby(x, theFunction);
