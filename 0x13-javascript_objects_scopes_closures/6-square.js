5b9044daca6b9b96ccbfb4708e0cf582b4b67db8
kwabenasapong <kwabenasapong@gmail.com>
2022-11-30 02:59:42 +0000

    Update task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,13 +11,13 @@ class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-      return super.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
+      c = 'X';
     }
-  }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+     }
+    }
 }
 
 module.exports = Square;
