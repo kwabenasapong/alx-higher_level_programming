cc8287882e3421dd460f24800227774122e7fb58
kwabenasapong <kwabenasapong@gmail.com>
2022-12-05 15:17:46 +0000

    Add task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' a script that lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    db = MySQLdb.connect(
+            'localhost',
+            mysql_username,
+            mysql_password,
+            database_name
+            )
+    cur = db.cursor()
+    sql = "SELECT id, name FROM hbtn_0e_0_usa.states ORDER BY id ASC"
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print("{}".format(row))
+    cur.close()
+    db.commit()
+    db.close()
