2865528497af4bc91c9483c60300b6f981774376
kwabenasapong <kwabenasapong@gmail.com>
2022-12-17 05:34:23 +0000

    Update task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
             print("[{}] {}".format(json.get('id'), json.get('name')))
         else:
             print("No result")
-    except requests.exceptions.RequestException:
+    except ValueError:
         print("Not a valid JSON")
